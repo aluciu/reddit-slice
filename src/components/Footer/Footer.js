bbb73323cb0144920bd8ca9ced58c9f8f6ae8f7b
Aurel L <ironman@artlogo.ro>
2022-07-27 16:43:02 +0300

    style footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,10 @@ export function Footer() {
     <footer className={styles.footer}>
       <ul>
         <li><a href="https://github.com/aluciu">GitHub</a></li>
-        <li><a href="https://www.codecademy.com/profiles/aluciu">CodeAcademy</a></li>
+        <li><a href="https://www.codecademy.com/profiles/aluciu">CodeCademy</a></li>
         <li><a href="https://www.aluciu.ro/">aluciu.ro</a></li>
       </ul>
+      <p>Project for CodeCademy Course</p>
     </footer>
   );
 }
